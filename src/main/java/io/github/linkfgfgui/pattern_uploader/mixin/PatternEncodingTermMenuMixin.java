7cfba682505568c879a89668acafa55a1990a899
link-fgfgui <fgfgui_link@outlook.com>
2026-03-21 12:13:19 +0800

    refactor: Updated key identification logic to ensure compatibility with EAEP.

--- src/main/java/io/github/linkfgfgui/pattern_uploader/mixin/PatternEncodingTermMenuMixin.java
+++ src/main/java/io/github/linkfgfgui/pattern_uploader/mixin/PatternEncodingTermMenuMixin.java
@@ -3,15 +3,19 @@ package io.github.linkfgfgui.pattern_uploader.mixin;
 import appeng.menu.AEBaseMenu;
 import appeng.menu.me.items.PatternEncodingTermMenu;
 import io.github.linkfgfgui.pattern_uploader.network.IPatternEncodingIdSync;
+import io.github.linkfgfgui.pattern_uploader.network.UploadInventoryPatternsToProvidersC2SPacket;
 import io.github.linkfgfgui.pattern_uploader.utils.RecipeFinderUtilEMI;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
+import net.neoforged.neoforge.network.PacketDistributor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static io.github.linkfgfgui.pattern_uploader.PatternUploader.recipeIdString;
@@ -27,6 +31,15 @@ public abstract class PatternEncodingTermMenuMixin implements IPatternEncodingId
         this.eap$pendingRecipeIdUpload = id;
     }
 
+    @Inject(method = "encode", at = @At("HEAD"), remap = false, cancellable = true)
+    private void onEncode(CallbackInfo ci) {
+        if (((PatternEncodingTermMenu) (Object) this).isClientSide()) {
+            if (Screen.hasAltDown()){
+                PacketDistributor.sendToServer(new UploadInventoryPatternsToProvidersC2SPacket());
+                ci.cancel();
+            }
+        }
+    }
 
     @Inject(method = "encodePattern", at = @At("TAIL"), remap = false, cancellable = true)
     private void eap$writeEncodePlayerToPattern(CallbackInfoReturnable<ItemStack> cir) {
